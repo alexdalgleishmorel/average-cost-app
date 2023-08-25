4242ec516472feae65d6e278645e488e18d8671c
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2023-08-25 16:04:49 -0600

    Fixed header safe zone

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'average-cost',
+  appId: 'com.alexdalgleishmorel.averagecostedge',
+  appName: 'ACE',
   webDir: 'www',
   server: {
     androidScheme: 'https'
