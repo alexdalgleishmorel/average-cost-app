9fc1b2622626eb60bfb3b00b4e81c14a40ffb9f6
alexdalgleishmorel <alex.dalgleishmorel@gmail.com>
2023-11-16 12:25:38 -0700

    api-key-validation-modal component documentation

--- src/app/components/api-key-validation-modal/api-key-validation-modal.component.ts
+++ src/app/components/api-key-validation-modal/api-key-validation-modal.component.ts
@@ -12,8 +12,11 @@ export class ApiKeyValidationModalComponent implements OnInit {
   @Input() apiKeyValue: string = '';
   public validationStatus: ApiKeyValidationStatus = ApiKeyValidationStatus.IN_PROGRESS;
 
-  constructor(private modalCtrl: ModalController, private http: HttpClient) { }
+  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
 
+  /**
+   * Immediately begins api key validation on component intializing. Sets the validation status flag upon completion.
+   */
   ngOnInit() {
     let url = `${ALPHA_VANTAGE_API_URL}?function=${STOCK_FUNCTION_TYPE}&symbol=CSCO&outputsize=full&apikey=${this.apiKeyValue}`;
     this.http.get(url).subscribe({
@@ -30,18 +33,34 @@ export class ApiKeyValidationModalComponent implements OnInit {
     });
   }
 
+  /**
+   * Dismisses the modal after a successful validation.
+   */
   exitWithSuccess() {
     this.modalCtrl.dismiss(true);
   }
 
+  /**
+   * Dismisses the modal after a failed validation.
+   */
   exitWithFailure() {
     this.modalCtrl.dismiss(false);
   }
 
+  /**
+   * Determines whether the api key validation is still in progress
+   * 
+   * @returns {boolean} Whether the validation is still in progress
+   */
   validationInProgress(): boolean {
     return this.validationStatus === ApiKeyValidationStatus.IN_PROGRESS;
   }
 
+  /**
+   * Determines whether the api key validation is complete and successful
+   * 
+   * @returns {boolean} Whether the validation is complete and was successful
+   */
   validationSuccess(): boolean {
     return this.validationStatus === ApiKeyValidationStatus.SUCCESS;
   }
